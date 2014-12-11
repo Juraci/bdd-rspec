1d80ffe49ac2fda3c5784132683dc91d94568064
Juraci <juraci.vieira@gmail.com>
2014-12-11 18:48:57 -0200

    Refactor: extracting method to class method

--- lib/calc.rb
+++ lib/calc.rb
@@ -10,7 +10,7 @@ class Calc
     final_value = ""
     @screen.each  do |current_digit|
       if current_digit == EQUAL_SYMBOL
-        final_value = sum(final_value)
+        final_value = Sum.new.exec(final_value)
       else
         final_value += current_digit.to_s
       end
@@ -19,12 +19,6 @@ class Calc
   end
 
   def sum(final_value)
-    total = 0
-    numbers = final_value.split('+')
-    numbers.each do |number|
-      total = total + number.to_i
-    end
-    total
   end
 
   def type(digit)
@@ -33,5 +27,16 @@ class Calc
 
   def present(number)
     number.to_i
+  end
+end
+
+class Sum
+  def exec(value)
+    total = 0
+    numbers = value.split('+')
+    numbers.each do |number|
+      total = total + number.to_i
+    end
+    total
   end
 end
