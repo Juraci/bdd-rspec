99e6b4202211be78008d35ca38a88516dae3e57c
Juraci <juraci.vieira@gmail.com>
2014-12-22 17:47:26 -0200

    Sum working for all scenarios

--- spec/calc/sum_spec.rb
+++ spec/calc/sum_spec.rb
@@ -1,24 +1,31 @@
 require 'spec_helper'
 
 describe 'Sum' do
+  let(:sum) { Calc::Sum.new }
+
   describe '#exec' do
     context 'when it receives 1 + 23' do
       it 'returns 24' do
-        sum = Calc::Sum.new
         expect(sum.exec("1 + 23")).to eq(24)
       end
     end
 
     context 'when it receives 1 + -1' do
-      it 'returns 0'
+      it 'returns 0' do
+        expect(sum.exec("1 + -1")).to eq(0)
+      end
     end
 
     context 'when it receives 1 + -1 + 15' do
-      it 'returns 15'
+      it 'returns 15' do
+        expect(sum.exec("1 + -1 + 15")).to eq(15)
+      end
     end
 
     context 'when it receives 1 + -1 + -15' do
-      it 'returns -15'
+      it 'returns -15' do
+        expect(sum.exec("1 + -1 + -15")).to eq(-15)
+      end
     end
   end
 end
